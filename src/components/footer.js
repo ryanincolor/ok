277791f7d321c34b194eca5555ee834f764d23e5
ryanincolor <ryan@madebyhuman.se>
2019-01-21 18:54:34 +0100

    add social links

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,12 +10,13 @@ const Footer = () => (
   		<a href="mailto:hello@madebyhuman.se">hello@madebyhuman.se</a>
   	</div>
   	<div className={styles.links}>
-  		<a href="https://www.instagram.com/orphanking_/">instagram</a>
-  		<a href="https://www.instagram.com/orphanking_/">facebook</a>
-  		<a href="https://www.instagram.com/orphanking_/">soundcloud</a>
-  		<a href="https://www.instagram.com/orphanking_/">spotify</a>
-  		<a href="https://www.instagram.com/orphanking_/">youtube</a>
-  		<a href="https://www.instagram.com/orphanking_/">apple music</a>
+  		<a href="https://www.instagram.com/orphanking_" target="_blank">instagram</a>
+  		<a href="https://www.facebook.com/orphankng" target="_blank">facebook</a>
+      <a href="https://twitter.com/orphanking_" target="_blank">twitter</a>
+  		<a href="https://soundcloud.com/orphanking" target="_blank">soundcloud</a>
+  		<a href="https://open.spotify.com/artist/7bFR4AY6BX3kFpFz6614xr?si=4pELlLShReKmPPZqGtDKZg" target="_blank">spotify </a>
+  		<a href="https://www.youtube.com/channel/UCxHg7zKbM5_e_jAy_tib6rA" target="_blank">youtube</a>
+  		<a href="https://itunes.apple.com/se/artist/orphan-king/1330385502?l=en" target="_blank">apple music</a>
   		<a href="https://www.instagram.com/orphanking_/">presskit</a>
   		<a href="https://www.instagram.com/orphanking_/">photos</a>
   	</div>
