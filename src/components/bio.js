1e6bf799edbc43bccb751c6ce9932d450b278f7f
ryanincolor <ryan@bravelikeus.com>
2019-04-06 19:00:05 +0200

    fix typos

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,17 +8,10 @@ const Bio = () => (
   <div className={styles.bio}>
   	<div>
   		<Title title="Bio"/>
-      	<p>Ominous and nostalgic, his music surfaces with dismal
-themes of religion, hope, betrayal, sexuality, and family.
-Often fusing emotional vocals that take the backseat
-to their synthetic counterparts, the music is an odyssey
-worth exploring, but the real story is revealed in the
-silence in between. Orphan King is the moniker of LA
-raised and Stockholm based producer Ryan Pittman.</p>
-      	<p>He grew up in a grim religious net, void of normal life. Eventually his father pulled him out at the expense of losing their family. They hit the road, moving around numerous times before finally settling down in Los Angeles. The battle with abusive piety and his ephemeral living conditions had a huge impact on his life and can subliminally be heard in his music.</p>
+      	<p>Ominous and nostalgic, his music surfaces with dismal themes of religion, hope, betrayal, sexuality, and family. Often fusing emotional vocals that take the backseat to their synthetic counterparts, the music is an odyssey worth exploring, but the real story is revealed in the silence in between. Orphan King is the moniker of LA raised and Stockholm based producer Ryan Pittman.</p>
       	<p>He was born into in a grim hyper religious houshold, spending most of his days seperated from normal childhood life. He eventually was taken out of it and spent the greater part of his life moving from place to place. The battle with abusive piety and his ephemeral living conditions is a theme that is cryptically heard through out his music.</p>
-      	<p>Early as 8, he was fixated on artists like The Cure, Bauhaus and Pink Floyd. He picked up his first guitar at 13,formed a handful of unsuccessful hardcore and punk bands, but it was the underground Los Angeles rave scene and later his years in Berlin that ultimately drove him towards electronic music. He felt at home with anonymity of the scene at the time. </p>
-      	<p>In 2016, after years in working as a designer, he shifted focus to making music again. Over the next two years he would develop his sound, challenging and channeling his painful past. Rising somewhere between the emotional and the obcene, Orphan King was materialized. In 2018 he self released his debut EP called Witness. Jan 2019 he announced “Kids” a 3-part album that will be released over the year.</p>
+      	<p>Early as 8, he was fixated on artists like The Cure, Bauhaus and Pink Floyd. He picked up his first guitar at 13, formed a handful of unsuccessful hardcore and punk bands, but it was the underground Los Angeles rave scene and later his years in Berlin that ultimately drove him towards electronic music. He felt at home with anonymity of the scene at the time. </p>
+      	<p>In 2016, after years working as a designer, he shifted focus to making music again. Over the next two years he would develop his sound, challenging and channeling his painful past. Rising somewhere between the emotional and the obcene, Orphan King was materialized. In 2018 he self released his debut EP called Witness. Jan 2019 he announced “Kids” a 3-part album that will be released over the year.</p>
   	</div>
   </div>
 )
