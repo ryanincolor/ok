3aa39ee7308ac692bd6519fac96bd82665383ff7
ryanincolor <ryan@madebyhuman.se>
2019-04-11 17:40:06 +0200

    update soundcloud links

--- src/components/music.js
+++ src/components/music.js
@@ -14,14 +14,14 @@ const Music = () => (
             flexWrap: 'wrap',
           }} 
         >
+          <MusicPlayer url="soundcloud.com/tracks/603385224"/>
           <MusicPlayer url="soundcloud.com/tracks/294719977"/>
-          <MusicPlayer url="soundcloud.com/tracks/297514738"/>
           <MusicPlayer url="soundcloud.com/tracks/556875138%3Fsecret_token%3Ds-Ox1h8"/>
+          <MusicPlayer url="soundcloud.com/tracks/297514738"/>
+          <MusicPlayer url="soundcloud.com/tracks/378293723"/>
           <MusicPlayer url="soundcloud.com/tracks/557506665%3Fsecret_token%3Ds-0KqwT"/>
           <MusicPlayer url="soundcloud.com/tracks/378293741"/>
           <MusicPlayer url="soundcloud.com/tracks/306353116"/>
-          <MusicPlayer url="soundcloud.com/tracks/310813194"/>
-          <MusicPlayer url="soundcloud.com/tracks/378293723"/>
        </div>
   </div>
 )
